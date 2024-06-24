34eb91685bd883a0f81b65987281f42939f9f479
Sieur Loic <loic.tramis@gmail.com>
2024-06-24 17:25:58 +0200

    Fix: /api/cohorts routing

--- server/routes/api.cohorts.routes.js
+++ server/routes/api.cohorts.routes.js
@@ -2,8 +2,8 @@ const router = require("express").Router();
 const path = require("path");
 const cohorts = require("../cohorts.json");
 
-router.get("/api/cohorts", (req, res, next) => {
-  res.json(cohorts);
+router.get("/", (req, res, next) => {
+    res.json(cohorts);
 });
 
 module.exports = router;
