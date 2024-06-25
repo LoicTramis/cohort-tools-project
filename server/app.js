4b17ff51b5b1bfe88c121d9461f98da168c47e27
Sieur Loic <loic.tramis@gmail.com>
2024-06-25 17:16:50 +0200

    Clean: schemas

--- server/app.js
+++ server/app.js
@@ -4,86 +4,20 @@ const cookieParser = require("cookie-parser");
 const PORT = 5005;
 const cors = require("cors");
 const mongoose = require("mongoose");
-const { Schema } = mongoose;
 
 mongoose
-  .connect("mongodb://127.0.0.1:27017/cohort-tools-api")
-  .then((x) => console.log(`Connected to Database: "${x.connections[0].name}"`))
-  .catch((err) => console.error("Error connecting to MongoDB", err));
-
-//Mongoose Schemas
-const cohortSchema = new Schema({
-  cohortSlug: { type: String, unique: true }, // String is shorthand for {type: String}
-  cohortName: { type: String, required: true },
-  program: {
-    type: String,
-    enum: ["Web Dev", "UX/UI", "Data Analytics", "Cybersecurity"],
-  },
-  format: { type: String, enum: ["Full Time", "Part Time"] },
-  campus: {
-    type: String,
-    enum: [
-      "Madrid",
-      "Barcelona",
-      "Miami",
-      "Paris",
-      "Berlin",
-      "Amsterdam",
-      "Lisbon",
-      "Remote",
-    ],
-  },
-  startDate: { type: Date, default: Date.now },
-  endDate: { type: Date },
-  inProgress: { type: Boolean, default: false },
-  programManager: { type: String, required: true },
-  leadTeacher: { type: String, required: true },
-  totalHours: { type: Number, default: 360 },
-});
-
-const studentSchema = new Schema({
-  firstName: { type: String, required: true },
-  lastName: { type: String, required: true },
-  email: { type: String, required: true, unique: true },
-  phone: { type: String, required: true },
-  linkedinUrl: { type: String, default: "" },
-  languages: {
-    type: [String],
-    enum: [
-      "English",
-      "Spanish",
-      "French",
-      "German",
-      "Portuguese",
-      "Dutch",
-      "Other",
-    ],
-  },
-  program: {
-    type: String,
-    enum: ["Web Dev", "UX/UI", "Data Analytics", "Cybersecurity"],
-  },
-  background: { type: String, default: "" },
-  image: { type: String, default: "https://i.imgur.com/r8bo8u7.png" },
-  cohort: mongoose.ObjectId,
-  projects: Array,
-});
-
-//Export Mongoose models
-let CohortModel = mongoose.model("cohort", cohortSchema); //creating a Model
-module.exports = CohortModel; //Exporting the created module.
-
-let StudentModel = mongoose.model("student", studentSchema); //creating a Model
-module.exports = StudentModel; //Exporting the created module.
+    .connect("mongodb://127.0.0.1:27017/cohort-tools-api")
+    .then((x) => console.log(`Connected to Database: "${x.connections[0].name}"`))
+    .catch((err) => console.error("Error connecting to MongoDB", err));
 
 // INITIALIZE EXPRESS APP - https://expressjs.com/en/4x/api.html#express
 const app = express();
 
 // MIDDLEWARE
 app.use(
-  cors({
-    origin: ["http://localhost:5173"], // add more if necessary
-  })
+    cors({
+        origin: ["http://localhost:5173"], // add more if necessary
+    })
 );
 app.use(express.json());
 app.use(morgan("dev"));
@@ -96,10 +30,10 @@ app.use("/api/cohorts", require("./routes/api.cohorts.routes.js"));
 app.use("/api/students", require("./routes/students.routes"));
 
 app.get("/docs", (req, res) => {
-  res.sendFile(__dirname + "/views/docs.html");
+    res.sendFile(__dirname + "/views/docs.html");
 });
 
 // START SERVER
 app.listen(PORT, () => {
-  console.log(`Server listening on port ${PORT}`);
+    console.log(`Server listening on port ${PORT}`);
 });
